92276b593ff48cbfe9345b01953863ae84a67e7e
ASO2021 <aliciasuarezortiz@gmail.com>
2022-03-29 00:06:45 -0300

    Se agrega el Proyecto

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https:/saucedemo.com");
+        }
+    }
+}
